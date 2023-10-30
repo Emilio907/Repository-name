317059c17cb2beed7719cb75c9bdc38f99e500bd
Emilio907 <149433354+Emilio907@users.noreply.github.com>
2023-10-30 23:44:08 +0300

    Тема 1 Py

---
+++ Тема 1 Py/Простые типы/Арифметические операторы и избегание магических чисел.py
@@ -0,0 +1,8 @@
+# переменные лучше, чем "магические числа"
+# TODO Задайте длину и ширину футбольного поля
+
+perimeter = 280  # TODO Посчитайте периметр поля
+print("Периметр футбольного поля равен", perimeter)
+length = 90
+width = 50
+perimeter = length+width+length+width
